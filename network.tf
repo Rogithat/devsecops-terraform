9bfa1e6e9f47ac2dc5df4c3c0f90b6ce8fdfec9d
Igor Santos <isferreira@stefanini.com>
2022-02-16 15:35:32 -0300

    fix: terraform network

--- network.tf
+++ network.tf
@@ -1,25 +1,25 @@
+#odule "vpc" {
+ # source  = "terraform-google-modules/network/google//modules/vpc"
+ # version = "~> 3.1.1"
+#
+ # project_id   = var.project_id
+ # network_name = var.subnetwork
+#
+ # shared_vpc_host = false
+#}
+
+
 module "vpc" {
-  source  = "terraform-google-modules/network/google//modules/vpc"
+  source  = "terraform-google-modules/network/google//modules/subnets-beta"
   version = "~> 3.1.1"
 
   project_id   = var.project_id
   network_name = var.subnetwork
 
-  shared_vpc_host = false
-}
-
-
-module "vpc_subnets" {
-  source  = "terraform-google-modules/network/google//modules/subnets-beta"
-  version = "~> 3.1.1"
-
-  project_id   = var.project_id
-  network_name = module.vpc.network_name
-
   subnets = [
     {
       subnet_name   = "subnet-01"
-      subnet_ip     = "10.100.0.0/21"
+      subnet_ip     = "10.100.0.0/24"
       subnet_region = "${var.region}"
     },
   ]
@@ -28,15 +28,15 @@ module "vpc_subnets" {
     subnet-01 = [
       {
         range_name    = "subnet-01-secondary-01"
-        ip_cidr_range = "10.100.1.0/24"
+        ip_cidr_range = "10.191.1.0/24"
       },
       {
         range_name    = "subnet-01-secondary-02"
-        ip_cidr_range = "10.100.2.0/24"
+        ip_cidr_range = "10.192.2.0/24"
       },
       {
         range_name    = "subnet-01-secondary-02"
-        ip_cidr_range = "10.100.3.0/24"
+        ip_cidr_range = "10.193.3.0/24"
       },
     ]
 
